ef954ab5cdfcefb5eacfacc9c87a1bd4923f8a96
Juan Garcia <38033917+jgarcia45@users.noreply.github.com>
2018-11-30 14:54:12 -0800

    Create chop.s

---
+++ Labs/chop.s
@@ -0,0 +1,36 @@
+# chop.s
+# chops off the end of a string after character 10
+#
+# Register Usage:
+#                   $s0 holds address of string
+
+   .data
+mystring: .asciiz "supercalifragilisticexbialadotous"
+newline: .asciiz "\n"
+
+   .text
+main:
+
+   # load string address
+   la $s0, mystring
+
+   # display the original string
+   move $a0, $s0
+   li $v0, 4
+   syscall
+
+   la $a0, newline
+   syscall
+
+   # insert null character at location 10
+   sb $zero, 10($s0)
+
+   # print the chopped off string - supercalif
+   move $a0, $s0
+   syscall
+
+   la $a0, newline
+   syscall
+
+   li $v0, 10
+   syscall
